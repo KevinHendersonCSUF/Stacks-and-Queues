092475dd57d46a29fce3e4fa2e6bea78e1e51d45
KevinHendersonCSUF <123698269+KevinHendersonCSUF@users.noreply.github.com>
2024-05-04 00:12:41 +0000

    Finished Project 3

--- stack _queue_template-2.cpp
+++ stack _queue_template-2.cpp
@@ -9,7 +9,6 @@
 #include <string>
 #include <vector>
 
-
 // Add class Stack here
 class Stack {
 public:
@@ -89,24 +88,25 @@ public:
     std::vector<char> stack;
     bool loop_break = false;
     int num = 0;
+    int zero_case = 0;
     for (int i = 0; i < par.length(); i++) {
       if (par[i] == '(' || par[i] == '{' || par[i] == '[') {
         stack.push_back(par[i]);
       } else if (par[i] == ')' || par[i] == '}' || par[i] == ']') {
         if (stack.size() == 0) {
           loop_break = true;
-          std::cout << 2 << std::endl;
-          break;
+          zero_case++;
+          num = num + zero_case;
         }
-        if (par[i] == ')' && stack.back() != '(') {
+        if (par[i] == ')' && stack.back() != '(' && stack.size() != 0) {
           loop_break = true;
           num++;
         }
-        if (par[i] == '}' && stack.back() != '{') {
+        if (par[i] == '}' && stack.back() != '{' && stack.size() != 0) {
           loop_break = true;
           num++;
         }
-        if (par[i] == ']' && stack.back() != '[') {
+        if (par[i] == ']' && stack.back() != '[' && stack.size() != 0) {
           loop_break = true;
           num++;
         } else {
@@ -114,14 +114,10 @@ public:
         }
       }
     }
-    if (loop_break == true) {
-      std::cout << num << std::endl;
-    }
-    if (loop_break == false && stack.size() == 0) {
-      std::cout << num << std::endl;
-    }
     if (loop_break == false && stack.size() != 0) {
       std::cout << stack.size() << std::endl;
+    } else {
+      std::cout << num << std::endl;
     }
   }
 
@@ -134,7 +130,7 @@ public:
       if (par[i] == '(' || par[i] == '{' || par[i] == '[') {
         stack.push_back(par[i]);
       } else if (par[i] == ')' || par[i] == '}' || par[i] == ']') {
-          if (stack.size() == 0) {
+        if (stack.size() == 0) {
           size_zero = 1;
           std::cout << score << std::endl;
           break;
@@ -153,8 +149,8 @@ public:
         }
       }
     }
-    if(size_zero != 1) {
-    std::cout << score << std::endl;
+    if (size_zero != 1) {
+      std::cout << score << std::endl;
     }
   }
 };
@@ -218,55 +214,55 @@ public:
 // Remove comments before testing and do not change anything in main function
 int main() {
 Stack s1;
-// s1.validPara("(([]))");
+s1.validPara("(([]))");
 s1.minPara("(([]))");
 s1.scorePara("(([]))");
-// s1.validPara("(([])");
-// std::cout<<s1.indexError("(([))")<<std::endl;
+s1.validPara("(([])");
+std::cout<<s1.indexError("(([))")<<std::endl;
 s1.minPara("(([])");
-// s1.validPara("(([{}))");
-// std::cout<<s1.indexError("(([[}])")<<std::endl;
+s1.validPara("(([{}))");
+std::cout<<s1.indexError("(([[}])")<<std::endl;
 s1.minPara("(([{}))");
 s1.scorePara("(([{}))");
-// std::cout<<s1.indexError("({}[]()[)")<<std::endl;
-// s1.validPara("(([))");
+std::cout<<s1.indexError("({}[]()[)")<<std::endl;
+s1.validPara("(([))");
 s1.minPara("(([))");
-// std::cout<<s1.indexError("[({)]")<<std::endl;
-// s1.validPara("(([{[{({})}]}]))");
+std::cout<<s1.indexError("[({)]")<<std::endl;
+s1.validPara("(([{[{({})}]}]))");
 s1.minPara("(([{[{({})}]}]))");
 s1.scorePara("(([{[{({})}]}]))");
-// s1.validPara("(([[{[{({})}]))");
-// s1.validPara("(([[{[{({})}]}])]))");
+s1.validPara("(([[{[{({})}]))");
+s1.validPara("(([[{[{({})}]}])]))");
 s1.scorePara("(([[{[{({})}]}])]))");
-// std::cout<<s1.indexError("(([[{{({})]}])]))")<<std::endl;
-// s1.validPara("(())");
-// s1.validPara("(())");
-// s1.validPara("void function(){}");
+std::cout<<s1.indexError("(([[{{({})]}])]))")<<std::endl;
+s1.validPara("(())");
+s1.validPara("(())");
+s1.validPara("void function(){}");
 s1.scorePara("void function(){}");
-// s1.validPara("void function(");
+s1.validPara("void function(");
 s1.minPara("void function(");
-// s1.validPara("void function(std::string expre){if(expre){return 1;}else{return 0;}}");
+s1.validPara("void function(std::string expre){if(expre){return 1;}else{return 0;}}");
 s1.scorePara("void function(std::string expre){if(expre){return 1;}else{return 0;}}");
-// s1.validPara("void function(std::string expre){if(expre){return 1;}else{return 0;}");
-// s1.validPara("void function(std::string expre){if(expre){return 1;else{return 0;");
+s1.validPara("void function(std::string expre){if(expre){return 1;}else{return 0;}");
+s1.validPara("void function(std::string expre){if(expre){return 1;else{return 0;");
 s1.minPara("void function(std::string expre){if(expre){return 1;else{return 0;");
-// std::cout<<s1.indexError("void function(std::string expre){if(expre){return 1;else{return 0;")<<std::endl;
-// Queue q1;
-// q1.enqueue("This is a secure message.");
-// q1.processMsg();
-// q1.enqueue("The product I received is damaged. What should I do?");
-// q1.processMsg();
-// q1.enqueue("I need assistance with setting up my new device");
-// q1.processMsg();
-// q1.enqueue("The website is not loading properly on my browser.");
-// q1.processMsg();
-// q1.enqueue("I accidentally placed the wrong order. Can it be canceled?");
-// q1.processMsg();
-// q1.enqueue("This is your project3. Have a happy thanksgiving!!! Hahaha");
-// q1.processMsg();
-// q1.enqueue("I forgot my password and can't reset it. Help, please! Do you provide technical support for mobile devices?");
-// q1.processMsg();
-// q1.enqueue("The software update is causing issues on my computer. The response time on your website is very slow.");
-// q1.processMsg();
+std::cout<<s1.indexError("void function(std::string expre){if(expre){return 1;else{return 0;")<<std::endl;
+Queue q1;
+q1.enqueue("This is a secure message.");
+q1.processMsg();
+q1.enqueue("The product I received is damaged. What should I do?");
+q1.processMsg();
+q1.enqueue("I need assistance with setting up my new device");
+q1.processMsg();
+q1.enqueue("The website is not loading properly on my browser.");
+q1.processMsg();
+q1.enqueue("I accidentally placed the wrong order. Can it be canceled?");
+q1.processMsg();
+q1.enqueue("This is your project3. Have a happy thanksgiving!!! Hahaha");
+q1.processMsg();
+q1.enqueue("I forgot my password and can't reset it. Help, please! Do you provide technical support for mobile devices?");
+q1.processMsg();
+q1.enqueue("The software update is causing issues on my computer. The response time on your website is very slow.");
+q1.processMsg();
 return 0;
 }
